300d14850693d7c95a17652efa5230cad88462e6
kurdoglov21-gif <Kurdoglov21@gmail.com>
2025-10-22 18:23:55 +0300

    Init Unity Project

---
+++ Assets/Scripts/AddColliders.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AddColliders : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (Transform child in transform)
+        { 
+            child.gameObject.AddComponent<MeshCollider>();
+        }
+    }
+}
